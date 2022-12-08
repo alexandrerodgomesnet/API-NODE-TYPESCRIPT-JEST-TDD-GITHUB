7b0647c8e7ab035f6a0c3be38deb685169456e54
alexandrerodgomesnet <alexandrerodgomes@hotmail.com>
2022-12-08 13:57:45 -0300

    Ajustes database mongo.

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,39 +1,52 @@
+import '@src/util/Module-alias';
 import { Server } from '@overnightjs/core';
 import { Application } from 'express';
 import bodyParser from 'body-parser';
-
 import { ForecastController } from '@src/controllers/forecast';
-import '@src/util/Module-alias';
+import * as database from '@src/server/data/database';
 
 export class SetupServer extends Server {
-    /*
-     * same as this.port = port, declaring as private here will
-     * add the port variable to the SetupServer instance
+  /*
+   * same as this.port = port, declaring as private here will
+   * add the port variable to the SetupServer instance
+   */
+  constructor(private port = 3000) {
+    super();
+  }
+
+  /*
+   * We use a different method to init instead of using the constructor
+   * this way we allow the server to be used in tests and normal initialization
+   */
+  public async init(): Promise<void> {
+    this.setupExpress();
+    this.setupControllers();
+    /**
+     * remover comentário quando ter o mongoose instalado na maquina
+     * await this.databaseSetup();
      */
-    constructor(private port = 3000) {
-        super();
-    }
+    
+  }
 
-    /*
-     * We use a different method to init instead of using the constructor
-     * this way we allow the server to be used in tests and normal initialization
-     */
-    public async init(): Promise<void> {
-        this.setupExpress();
-        this.setupControllers();
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+    this.setupControllers();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-        this.setupControllers();
-    }
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    private setupControllers(): void {
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 
-    public getApp(): Application {
-        return this.app;
-    }
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
+  }
 }
